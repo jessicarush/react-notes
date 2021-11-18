41114d7958765b366b4658698a28b807823e6717
jessicarush <jessica@13down.io>
2021-11-18 12:59:09 -0800

    Initial commit

---
+++ component_template.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Example.css';
+
+
+class Example extends Component {
+  static defaultProps = {
+  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="Example">
+
+      </div>
+    );
+  }
+}
+
+export default Example;
